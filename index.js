d59ff774b0b15751b515361de6a5bcc7687e6b76
Malik Kamran Abid <30325523+malikkamranabid@users.noreply.github.com>
2019-07-01 19:51:35 +0500

    [53] added package functionality for redis (#1)
    * [53] added package functionality
    
    * [53] removed un necessary things in gitignore
    
    * [53] eplace all the R5.out.err to R5.out.error
    
    * [53] added test script
    
    * [53] Updated package version
    
    * [53] added config param

---
+++ index.js
@@ -0,0 +1,172 @@
+/* eslint-disable brace-style, camelcase, semi */
+/* global R5 */
+
+module.exports = Redis;
+
+if (!global.R5) {
+  global.R5 = {
+    out: console
+  }
+}
+
+// Constructors
+
+function Redis (db, host, port, pass) {
+  this.db = db;
+  this.host = host;
+  this.port = port;
+  this.pass = pass;
+  this.ready = false;
+  this.connect();
+}
+
+// Public Methods
+// TODO: check if this.ready, else reconnect and/or queue?
+
+Redis.prototype.connect = function () {
+  if (this.ready) { return; }
+
+  this.client = require('redis').createClient({
+    host: this.host,
+    port: this.port,
+    password: this.pass,
+    db: this.db
+  });
+
+  let _this = this;
+  this.client.on('ready', function () {
+    R5.out.log(`Connected to Redis (db: ${this.db})`);
+    _this.ready = true;
+  });
+
+  this.client.on('error', function (err) {
+    R5.out.error(`Redis error: ${err}`);
+    _this.ready = false;
+    _this.connect();
+  });
+};
+
+Redis.prototype.get = function (key, callback) {
+  if (!callback) { callback = function () { }; }
+
+  this.client.get(key, function (err, data) {
+    if (err) { R5.out.error(err); }
+    callback(err, data);
+  });
+};
+
+Redis.prototype.set = function (key, value, key_expiration, callback) {
+  if (!callback) { callback = function () { }; }
+
+  if (typeof value === 'object') {
+    R5.out.log('Passing a string into set is recommended (currently passed in object)');
+    value = stringify(value);
+  }
+
+  if (key_expiration) {
+    if (value) {
+      this.client.set(key, value, 'EX', key_expiration, handle_data);
+    }
+    else {
+      this.client.setex(key, key_expiration, 'EX', handle_data);
+    }
+  }
+  else {
+    this.client.set(key, value, handle_data);
+  }
+
+  function handle_data (err, data) {
+    if (err) { R5.out.error(err); }
+    callback(err, data);
+  }
+};
+
+Redis.prototype.delete = function (key, callback) {
+  if (!callback) { callback = function () { }; }
+
+  this.client.del(key, function (err, data) {
+    if (err) { R5.out.error(err); }
+    callback(err, data);
+  });
+};
+
+Redis.prototype.get_list = function (key, callback) {
+  this.client.lrange(key, 0, -1, function (err, res) {
+    if (err) { R5.out.error(err); }
+    callback(err, res);
+  });
+};
+
+Redis.prototype.set_list = function (key, value, max_length, callback) {
+  let _this = this;
+
+  _this.client.llen(key, function (err, res, body) {
+    if (err) { return callback(err, res, body); }
+
+    if (max_length !== false && res >= max_length) {
+      _this.client.lpop(key, callback(err, res, body));
+    }
+
+    _this.client.rpush(key, value, callback(err, res, body));
+  });
+};
+
+Redis.prototype.delete_list = function (key, value, count, callback) {
+  this.client.lrem(key, count, value, callback);
+};
+
+Redis.prototype.set_set = function (key, value, callback) {
+  this.client.sadd(key, value, callback);
+};
+
+Redis.prototype.get_set = function (key, callback) {
+  this.client.smembers(key, callback);
+};
+
+Redis.prototype.pop_set = function (key, callback) {
+  this.client.spop(key, callback);
+};
+
+Redis.prototype.delete_set = function (key, value, callback) {
+  this.client.srem(key, value, callback);
+};
+
+Redis.prototype.delete_all = function () {
+  this.client.flushdb(function () { R5.out.log('Redis flushed'); });
+};
+
+Redis.prototype.increment = function (key, callback) {
+  if (!callback) { callback = function () { }; }
+
+  this.client.incr(key, function (err, data) {
+    if (err) { R5.out.error(err); }
+    callback(err, data);
+  });
+};
+
+Redis.prototype.decrement = function (key, callback) {
+  if (!callback) { callback = function () { }; }
+
+  this.client.decr(key, function (err, data) {
+    if (err) { R5.out.error(err); }
+    callback(err, data);
+  });
+};
+
+Redis.prototype.get_ttl = function (key, callback) {
+  if (!callback) { callback = function () { }; }
+
+  this.client.ttl(key, function (err, data) {
+    if (err) { R5.out.error(err); }
+    callback(err, data);
+  });
+};
+
+// Private Methods
+
+function stringify (value) {
+  try { JSON.stringify(value); }
+  catch (e) {
+    R5.out.error(`stringify failed at value: \n${value} \n\n with exception: \n${e}`);
+  }
+}
---
+++ test.js
@@ -0,0 +1,383 @@
+/* eslint-disable brace-style, camelcase, semi */
+/* eslint-env mocha */
+require('dotenv').config();
+
+var chai = require('chai');
+var expect = chai.expect;
+var redis;
+
+let config = {
+  db: 0,
+  host: 'localhost',
+  port: 'root',
+  pass: ''
+};
+
+redis = new (require('./index.js'))(config.db, config.host, config.port, config.pass);
+
+function check_no_error (err, callback) {
+  expect(err).to.be.a('null');
+  callback();
+}
+
+describe('Redis', () => {
+  describe('Connect', () => {
+    it('should connect and return no object', (done) => {
+      let connect = redis.connect();
+      expect(connect).to.be.an('undefined');
+      done();
+    });
+  });
+
+  describe('Set key value', () => {
+    describe('Set key value without expire time', () => {
+      let key = 'color';
+      let value = 'red';
+      let err, response;
+      redis.set(key, value, false, (error, data) => {
+        err = error;
+        response = data;
+      });
+
+      it('should not return error', (done) => {
+        check_no_error(err, done);
+      });
+
+      it('should return OK', (done) => {
+        expect(response).to.equal('OK');
+        done();
+      });
+    });
+
+    describe('Set key value with expire time', () => {
+      let key = 'hobby';
+      let value = 'driving';
+      let err, response;
+      redis.set(key, value, 100, (error, data) => {
+        err = error;
+        response = data;
+      });
+
+      it('should not return error', (done) => {
+        expect(err).to.be.a('null');
+        done();
+      });
+
+      it('should return OK', (done) => {
+        expect(response).to.equal('OK');
+        done();
+      });
+
+      it('should get null value', (done) => {
+        setTimeout(() => {
+          redis.get(key, (error, data) => {
+            expect(data).to.equal(null);
+            done();
+          });
+        }, 105);
+      });
+    });
+
+    describe('Set key and object value', () => {
+      let key = 'game';
+      let value = {
+        'name': 'spades'
+      };
+      let err, response;
+      redis.set(key, value, false, (error, data) => {
+        err = error;
+        response = data;
+      });
+
+      it('should not return error', (done) => {
+        expect(err).to.be.a('null');
+        done();
+      });
+
+      it('should return OK', (done) => {
+        expect(response).to.equal('OK');
+        done();
+      });
+    });
+  });
+
+  describe('Get key value', () => {
+    let key = 'color';
+    let err, response;
+    redis.get(key, (error, data) => {
+      err = error;
+      response = data;
+    });
+
+    it('should return value without an error', (done) => {
+      expect(err).to.be.a('null');
+      done();
+    });
+
+    it('should return value red', (done) => {
+      expect(response).to.equal('red');
+      done();
+    });
+  });
+
+  describe('Delete key', () => {
+    let key = 'color';
+    let err, getResponse, deleteResponse;
+    redis.delete(key, (error, data) => {
+      err = error;
+      deleteResponse = data;
+    });
+
+    it('should not return error', (done) => {
+      expect(err).to.be.a('null');
+      done();
+    });
+
+    it('should return integer value', (done) => {
+      expect(deleteResponse).to.be.a('number');
+      done();
+    });
+
+    redis.get(key, (error, data) => {
+      err = error;
+      getResponse = data;
+    });
+
+    it('should return null value after deletion', (done) => {
+      expect(getResponse).to.be.a('null');
+      done();
+    });
+  });
+
+  describe('Set list', () => {
+    describe('Set list without maximum length option', () => {
+      let key = 'mylist';
+      let value1 = 'one';
+      let err, response;
+      redis.set_list(key, value1, false, (error, value, data) => {
+        err = error;
+        response = value;
+      });
+
+      it('should not return error', (done) => {
+        expect(err).to.be.a('null');
+        done();
+      });
+
+      it('should return length of the list', (done) => {
+        expect(response).to.be.a('number');
+        done();
+      });
+    });
+
+    describe('Set list with maximum length option', () => {
+      let key = 'mylist';
+      let value2 = 'two';
+      let err, resp;
+      redis.set_list(key, value2, 1, (error, value, data) => {
+        err = error;
+        resp = value;
+      });
+
+      it('should not return error', (done) => {
+        expect(err).to.be.a('null');
+        done();
+      });
+
+      it('should return length of the list', (done) => {
+        expect(resp).to.be.a('number');
+        done();
+      });
+
+      it('should return length 1', (done) => {
+        expect(resp).to.equal(1);
+        done();
+      });
+    });
+  });
+
+  describe('Get list', () => {
+    let key = 'mylist';
+    let err, response;
+    redis.get_list(key, (error, data) => {
+      err = error;
+      response = data;
+    });
+
+    it('should not return error', (done) => {
+      expect(err).to.be.a('null');
+      done();
+    });
+
+    it('should return list values', (done) => {
+      expect(response).to.be.an('array');
+      done();
+    });
+  });
+
+  describe('Delete value from list', () => {
+    let key = 'mylist';
+    let value = 'one';
+    let count = 1;
+    let err, response;
+    redis.delete_list(key, value, count, (error, data) => {
+      err = error;
+      response = data;
+    });
+
+    it('should not return error', (done) => {
+      expect(err).to.be.a('null');
+      done();
+    });
+
+    it('should return list values', (done) => {
+      expect(response).to.be.a('number');
+      done();
+    });
+
+    it('should not include one', (done) => {
+      redis.get_list(key, (error, data) => {
+        expect(data).to.not.include('one');
+        done();
+      });
+    });
+  });
+
+  describe('Add values to set', () => {
+    let key = 'myset';
+    let value = 'one';
+    let err, response;
+    redis.set_set(key, value, (error, data) => {
+      err = error;
+      response = data;
+    });
+
+    it('should not return error', (done) => {
+      expect(err).to.be.a('null');
+      done();
+    });
+
+    it('should return number of elements added to the set', (done) => {
+      expect(response).to.be.a('number');
+      expect(response).to.equal(1);
+      done();
+    });
+  });
+
+  describe('Get set values', () => {
+    let key = 'myset';
+    let err, response;
+    redis.get_set(key, (error, data) => {
+      err = error;
+      response = data;
+    });
+
+    it('should not return error', (done) => {
+      expect(err).to.be.a('null');
+      done();
+    });
+
+    it('should return all the values of set', (done) => {
+      expect(response).to.be.an('array');
+      expect(response).to.eql(['one']);
+      done();
+    });
+  });
+
+  describe('Remove random value from set', () => {
+    let key = 'myset';
+    let err, response;
+    redis.pop_set(key, (error, data) => {
+      err = error;
+      response = data;
+    });
+
+    it('should not return error', (done) => {
+      expect(err).to.be.a('null');
+      done();
+    });
+
+    it('should remove one or more random values', (done) => {
+      expect(response).to.exist;
+      done();
+    });
+  });
+
+  describe('Delete key based value from set', () => {
+    let key = 'myset';
+    let value = 'one';
+    let err, response;
+    redis.delete_set(key, value, (error, data) => {
+      err = error;
+      response = data;
+    });
+
+    it('should not return error', (done) => {
+      expect(err).to.be.a('null');
+      done();
+    });
+
+    it('should return number of values removed from the set', (done) => {
+      expect(response).to.be.a('number');
+      done();
+    });
+  });
+
+  describe('Increment value', () => {
+    let key = 'age';
+    let value = 23;
+    let err, response;
+    redis.set(key, value, false, (error, data) => {
+      err = error;
+      response = data;
+    });
+    redis.increment(key, (error, data) => {
+      err = error;
+      response = data;
+    });
+
+    it('should not return error', (done) => {
+      expect(err).to.be.a('null');
+      done();
+    });
+
+    it('should return incremented value', (done) => {
+      expect(response).to.be.a('number');
+      expect(response).to.equal(value + 1);
+      done();
+    });
+  });
+
+  describe('Decrement value', () => {
+    let key = 'age';
+    let value = 24;
+    let err, response;
+
+    redis.set(key, value, false, (error, data) => {
+    });
+
+    redis.decrement(key, (error, data) => {
+      err = error;
+      response = data;
+    });
+
+    it('should not return error', (done) => {
+      expect(err).to.be.a('null');
+      done();
+    });
+
+    it('should return decremented value', (done) => {
+      expect(response).to.be.a('number');
+      expect(response).to.equal(23);
+      done();
+    });
+  });
+
+  describe('Delete all keys of current DB', () => {
+    redis.delete_all();
+
+    it('should delete all the keys', (done) => {
+      done();
+    });
+  });
+});

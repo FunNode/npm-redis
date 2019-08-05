ac8a004816148f632f9e8a17156478376033c354
Melek <bishoymelekwadie@gmail.com>
2019-08-05 08:47:57 +0200

    eslint indent only 2 spaces

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
     },
     rules: {
         'space-before-function-paren': 'error',
-        indent: ['error', 4],
+        indent: ['error', 2],
         'linebreak-style': ['error', 'unix'],
         quotes: ['error', 'single'],
         semi: ['error', 'always'],
